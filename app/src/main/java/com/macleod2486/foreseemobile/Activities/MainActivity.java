17a2024bc7655dfca6d5f78727c6cda044d1a778
macleod2486 <macleod2486@gmail.com>
2017-02-07 01:14:32 -0600

    Added in basic activity and license

---
+++ app/src/main/java/com/macleod2486/foreseemobile/Activities/MainActivity.java
@@ -0,0 +1,35 @@
+/*
+    ForeseeMobile
+    Copyright (C) 2017  macleod2486
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see [http://www.gnu.org/licenses/].
+ */
+
+package com.macleod2486.foreseemobile.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.macleod2486.foreseemobile.R;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    public void onCreate(Bundle instance)
+    {
+        super.onCreate(instance);
+
+        setContentView(R.layout.activity_main);
+    }
+}
